eaa0540e9a22dd8427ab0169b3f5eab8ca20982a
jPales1 <joaopedropales@gmail.com>
2024-11-30 19:13:40 -0300

    substitui componente Home por router-outlet para habilitar roteamento na aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './layout/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
