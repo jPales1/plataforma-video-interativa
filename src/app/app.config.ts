8fd38a15105f9b934a33b0f595379831dcf35b83
jPales1 <joaopedropales@gmail.com>
2024-11-30 18:28:05 -0300

    adiciona rotas para Home e detalhes do vídeo, e configura o HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideAuth0 } from '@auth0/auth0-angular';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAuth0({
-    domain: 'dev-pilytijhc2tbche2.us.auth0.com',
-    clientId: 'I2r224pUQuf5dpxM8rpn4rV8N3HJGFOz',
-    authorizationParams: { redirect_uri: window.location.origin, },
-  })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimationsAsync(),
+    provideAuth0({
+      domain: 'dev-pilytijhc2tbche2.us.auth0.com',
+      clientId: 'I2r224pUQuf5dpxM8rpn4rV8N3HJGFOz',
+      authorizationParams: { redirect_uri: window.location.origin, }
+    }),
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../app/layout/home/home.component';
+import { VideoDetailsComponent } from '../app/videos/video-details/video-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'video/:id', component: VideoDetailsComponent },
+  { path: '**', redirectTo: '' } 
+];
