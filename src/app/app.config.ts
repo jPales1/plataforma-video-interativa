eaea6d9765eaa6da2dbc8e9bf1efaa143fb366df
jPales1 <joaopedropales@gmail.com>
2024-11-30 17:20:25 -0300

    adiciona configuração do Auth0 à aplicação

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAuth0({
+    domain: 'dev-pilytijhc2tbche2.us.auth0.com',
+    clientId: 'I2r224pUQuf5dpxM8rpn4rV8N3HJGFOz',
+    authorizationParams: { redirect_uri: window.location.origin, },
+  })]
 };
