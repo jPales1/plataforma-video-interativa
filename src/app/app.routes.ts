b721e54a0fc9091387f5faf32145afd8ce618127
jPales1 <joaopedropales@gmail.com>
2024-11-30 19:13:44 -0300

    adiciona componente NotFound e atualiza roteamento para exibir página de erro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from '../app/layout/home/home.component';
-import { VideoDetailsComponent } from '../app/videos/video-details/video-details.component';
+import { HomeComponent } from './layout/home/home.component';
+import { VideoDetailsComponent } from './videos/video-details/video-details.component';
+import { NotFoundComponent } from './layout/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'video/:id', component: VideoDetailsComponent },
-  { path: '**', redirectTo: '' } 
+  { path: '**', component: NotFoundComponent }
 ];
