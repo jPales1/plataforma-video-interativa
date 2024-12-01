576d60178cc94868dd231105ea902958d189280b
jPales1 <joaopedropales@gmail.com>
2024-11-30 22:10:08 -0300

    refatora VideoService para corrigir URLs da API e adiciona método para incrementar visualizações

--- src/app/video.service.ts
+++ src/app/video.service.ts
@@ -1,32 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideoService {
-  private apiUrl = 'http://localhost:3000/videos';
+  private apiUrl = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getVideos(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any[]>(`${this.apiUrl}/videos`);
   }
 
   getVideo(id: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${id}`);
+    return this.http.get<any>(`${this.apiUrl}/videos/${id}`);
   }
 
   addVideo(video: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, video);
+    return this.http.post<any>(`${this.apiUrl}/videos`, video);
   }
 
   updateVideo(video: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${video.id}`, video);
+    return this.http.put<any>(`${this.apiUrl}/videos/${video.id}`, video);
   }
 
   deleteVideo(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/videos/${id}`);
+  }
+
+  incrementViews(videoId: string): Observable<any> {
+    return this.getVideo(Number(videoId)).pipe(
+      switchMap(video => {
+        const updatedViews = video.views + 1;
+        return this.http.patch(`${this.apiUrl}/videos/${videoId}`, { views: updatedViews });
+      })
+    );
   }
 }
